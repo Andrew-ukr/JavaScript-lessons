217a16102110028ee2ce02651ac36bf86d96e4c0
Andrii <pashulyaand@gmail.com>
2020-11-12 21:57:39 +0200

    lessons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+
+let menuBtn1 = document.querySelectorAll('[class="item-link"]');
+
+
+// menuBtn1.forEach(element => {
+//   element.addEventListener('click', () => {
+//     element.classList.toggle('active');
+//   });
+// });
+
+let btn = document.querySelectorAll('.btn-list');
+
+btn.addEventListener('click', (e) => {
+  if (e.target && e.target.tagName == 'BUTTON') {
+    console.log('aaaaaaa');
+  } 
+});
+
+
