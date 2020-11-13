2a55d7d2d14d9efbc399f4847ab3373ad3baf1f9
Andrii <pashulyaand@gmail.com>
2020-11-13 23:17:05 +0200

    final practice

--- js/script.js
+++ js/script.js
@@ -3,5 +3,25 @@
 window.addEventListener('DOMContentLoaded', () => {
   //_____________________________________________________
 
+  let btn = document.querySelector('button.btn_white');
+
+  // btn.addEventListener('click', () => {
+  //   let delay = setTimeout(modalWindow, 2000);
+  // });
+  
+  // btn.addEventListener('click', () => {
+  //   let delay = setInterval(modalWindow, 2000);
+  // });
+
+let delay = setTimeout(function name() {
+  console.log('hello');
+  delay = setTimeout(name, 500);
+}, 500);
+
+
+  function modalWindow() {
+    console.log('hello !!!');
+  }
+
   //_____________________________________________________
 });
