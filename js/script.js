19bcff48ee121fb11881151d9f1c51e007ccac50
Andrii <pashulyaand@gmail.com>
2020-11-13 22:22:49 +0200

    start

--- js/script.js
+++ js/script.js
@@ -2,44 +2,6 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   //_____________________________________________________
-  let tabContent = document.querySelectorAll('.tabcontent');
-  let tabheaderItems = document.querySelector('.tabheader__items');
-  let tabheaderItem = document.querySelectorAll('.tabheader__item');
 
-  function hide() {
-    tabContent.forEach(element => {
-      // element.style.display = 'none';
-      element.classList.remove('show');
-      element.classList.add('hide');
-    });
-
-    tabheaderItem.forEach(element => {
-      element.classList.remove('tabheader__item_active');
-    });
-  }
-
-  function show(i = 0) {
-    // tabContent[i].style.display = 'block';
-    tabContent[i].classList.remove('hide');
-    tabContent[i].classList.add('show', 'animation');
-    tabheaderItem[i].classList.add('tabheader__item_active');
-  }
-
-  function getTabheaderItem() {
-    tabheaderItems.addEventListener('click', (e) => {
-      if (e.target && e.target.classList.contains('tabheader__item')) {
-        tabheaderItem.forEach((element, i) => {
-          if (element === e.target) {
-            hide();
-            show(i);
-          }
-        });
-      }
-    });
-  }
-
-  hide();
-  show();
-  getTabheaderItem();
   //_____________________________________________________
 });
