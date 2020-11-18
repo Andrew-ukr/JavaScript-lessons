77c5b108e84da6ce8c82b198513027af56fb9907
Andrii <pashulyaand@gmail.com>
2020-11-18 14:51:24 +0200

    sfdfdsf

--- js/script.js
+++ js/script.js
@@ -3,8 +3,19 @@
 window.addEventListener('DOMContentLoaded', () => {
   //_____________________________________________________
 
+  let a = 1;
+  let b;
 
-  
+  let prom = new Promise((resolve, reject) => {
+    let prom1 = setTimeout(() => {
+      b = 2;
+      console.log('545544');
+    }, 2000);
+    resolve(prom1);
+  });
+  prom.then()
+
+  console.log(a + b);
 
   //_____________________________________________________
 });
